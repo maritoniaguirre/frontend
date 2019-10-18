08e9a228482a894392b75269bf10f3acb4cc7540
maritoniaguirre <maritoni92@gmail.com>
2019-10-17 21:51:36 -0500

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Route} from 'react-router-dom'
+import popper from 'popper.js';
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
+import './App.css'
+
+
+import Navbar from './components/Navbar/Navbar'
+import ETeacher from './components/eTeacher/eTeacher'
+import EStudent from './components/eStudent/eStudent'
+import ETeacherPerfil from './components/eTeacherPerfil/eTeacherPerfil'
+import EStudentPerfil from './components/eStudentPerfil/eStudentPerfil'
+import ETeacherRegistro from './components/eTeacherRegistro/eTeacherRegistro'
+import EStudentRegistro from './components/eStudentRegistro/eStudentRegistro'
+import EStudentPrograma from './components/eStudentPrograma/eStudentPrograma'
+import misClases from './components/misClases/misClases'
+import index from './components/Index/Index.jsx'
+import Teachers from './components/Teachers/Teachers'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar/>
+        <main className=" content-app">
+          <Route exact path='/' component={index}/>
+          <Route exact path='/eTeacher' component={ETeacher}/>
+          <Route exact path='/eStudent' component={EStudent}/>
+          <Route exact path='/eTeacherPerfil' component={ETeacherPerfil}/>
+          <Route exact path='/eStudentPerfil' component={EStudentPerfil}/>
+          <Route exact path='/eTeacherRegistro' component={ETeacherRegistro}/>
+          <Route exact path='/eStudentRegistro' component={EStudentRegistro}/>
+          <Route exact path='/eStudentPrograma' component={EStudentPrograma}/>
+          <Route exact path='/misClases' component={misClases}/>
+          <Route exact path='/teachers' component={Teachers}/>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
