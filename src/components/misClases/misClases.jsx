407b2f260e42caa805827a8d478b8f13c33c1a4d
maritoniaguirre <maritoni92@gmail.com>
2019-10-22 13:35:15 -0500

    proyectoFrontend

--- src/components/misClases/misClases.jsx
+++ src/components/misClases/misClases.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 import './misClases.css'
+import Perfil1 from '../../assets/img/química.jpg'
+import Perfil2 from '../../assets/img/derecho.jpg'
+import Perfil3 from '../../assets/img/física.jpg'
+import Perfil4 from '../../assets/img/Canva - Man In Crew-neck Shirt .png'
+import Facetime from '../../assets/img/apple.png'
+import Skype from '../../assets/img/skype.png'
 
 export class misClases extends Component {
     render() {
@@ -16,8 +22,8 @@ export class misClases extends Component {
                     <li>Profesor: Juan Castro</li>
                     <li>Área: Inglés</li>
                 </ul>
-            <img src="" className= "rounded-circle" style= {{width:"150px"}} alt="fotodeperfil1"/></div>
-            <div className= "col-md-6"><img src="" className= "rounded-circle" style= {{width:"250px"}} alt=""/></div>
+            <img src={Perfil1} className= "rounded-circle" style= {{width:"150px"}} alt="fotodeperfil1"/></div>
+            <div className= "col-md-6"><img src={Facetime} className= "rounded-circle" style= {{width:"250px"}} alt=""/></div>
         </div>
 
         <br></br>
@@ -29,7 +35,7 @@ export class misClases extends Component {
                         <li>Profesor: Jessica Ramírez</li>
                         <li>Área: Cálculo</li>
                     </ul>
-                <img src="./Imagenes/derecho.jpg" className= "rounded-circle" style= {{width:"150px"}} alt="fotodeperfil1"/></div>
+                <img src={Perfil2} className= "rounded-circle" style= {{width:"150px"}} alt="fotodeperfil1"/></div>
                 <div className= "col-md-6"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.8802629552843!2d-99.16440904942911!3d19.417578786828546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff3980d3bf21%3A0xdabaf4e144a4e7af!2sImpact%20Hub!5e0!3m2!1ses!2smx!4v1568324838739!5m2!1ses!2smx" width="400" height="300" frameborder="0" style={{border:"0"}} allowfullscreen=""></iframe></div>
             </div>
 
@@ -42,8 +48,8 @@ export class misClases extends Component {
                             <li>Profesor: John Stewart</li>
                             <li>Área: Historia</li>
                         </ul>
-                    <img src="" className= "rounded-circle" style={{width:"150px"}} alt="fotodeperfil1"/></div>
-                    <div className= "col-md-6"><iframe src="" width="400" height="300" frameborder="0" style={{border:"0"}} allowfullscreen=""></iframe></div>
+                    <img src={Perfil3} className= "rounded-circle" style={{width:"150px"}} alt="fotodeperfil1"/></div>
+                    <div className= "col-md-6"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.0905319292738!2d-99.26681604942993!3d19.365232286858518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d20734cf199eef%3A0xd532213440f5d4d!2sGarden%20Santa%20Fe!5e0!3m2!1ses!2smx!4v1568311883834!5m2!1ses!2smx" width="400" height="300" frameborder="0" style={{border:"0"}} allowfullscreen=""></iframe></div>
                 </div>
 
             <br></br>
@@ -55,8 +61,8 @@ export class misClases extends Component {
                                 <li>Profesor: Raúl Sánchez</li>
                                 <li>Área: Filosofía</li>
                             </ul>
-                        <img src="" className= "rounded-circle" style= {{width:"150px"}} alt="fotodeperfil1"/></div>
-                        <div className= "col-md-6"><img src="" style= {{width:"250px"}} alt=""/></div>
+                        <img src={Perfil4} className= "rounded-circle" style= {{width:"150px"}} alt="fotodeperfil1"/></div>
+                        <div className= "col-md-6"><img src={Skype} style= {{width:"250px"}} alt=""/></div>
                     </div>
                 
             </div>
